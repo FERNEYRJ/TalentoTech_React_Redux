4d3c7577ee6417b003cfabfaa7903aecc0e02ee1
FERNEY ROJAS <46285449+FERNEYRJ@users.noreply.github.com>
2024-03-12 20:47:40 -0500

    Update_2

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Example from "./components/example";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import UserList from "./components/user/UserList";
+
+function App() {
+  return (
+    <>      
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Example />} />
+          <Route path="/user" element={<UserList />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
