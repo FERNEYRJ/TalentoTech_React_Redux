10af9fb16a2ae6d0fae3468e660c306fee9a0ea4
FERNEY ROJAS <46285449+FERNEYRJ@users.noreply.github.com>
2024-03-15 21:03:00 -0500

    Update_4

--- src/components/user/UserList.jsx
+++ src/components/user/UserList.jsx
@@ -1,36 +1,48 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function UserList(){
+    /** Obtiene el estado de una variable */
+    const users = useSelector(state => state.users)
+    console.log(users)
 
     return (
         <div className="flex justify-center py-8 px-10">
         <table className="table-auto w-full">
             <thead>
-                <tr>
-                    <th className="px-4 py-2 bg-slate-500 text-white">Name</th>
-                    <th className="px-4 py-2 bg-slate-500 text-white">LastName</th>
-                    <th className="px-4 py-2 bg-slate-500 text-white">Email</th>
-                    <th className="px-4 py-2 bg-slate-500 text-white">Idetification</th>
-                    <th className="px-4 py-2 bg-slate-500 text-white">Avatar</th>
+                <tr className="bg-slate-500">
+                    <th className="px-4 py-2 text-white">Name</th>
+                    <th className="px-4 py-2 text-white">LastName</th>
+                    <th className="px-4 py-2 text-white">Email</th>
+                    <th className="px-4 py-2 text-white">Idetification</th>
+                    <th className="px-4 py-2 text-white">Avatar</th>
+                    <th className="px-4 py-2 text-white">Actions</th>
                 </tr>
             </thead>
-            <tbody className="bg-gray-200">
-                <tr>
-                    <td className="border px-4 py-2">Juan</td>
-                    <td className="border px-4 py-2">Perez</td>
-                    <td className="border px-4 py-2">juan@correo.com</td>
-                    <td className="border px-4 py-2">124545</td>
-                    <td className="border px-4 py-2">
-                        <img className="size-40 transition-transform duration-300 transform hover:scale-110"
-                            src="https://img.olympics.com/images/image/private/t_s_w960/t_s_16_9_g_auto/f_auto/primary/jxn3zttbkrvxudzifk6b"/></td>
+            <tbody className="bg-gray-200 ">
+                {users.map(user => (
+                <tr key={user._id}>
+                    <td className="border-y-2 px-4 py-2 border-indigo-600">{user.name}</td>
+                    <td className="border-y-2 px-4 py-2 border-indigo-600">{user.lastname}</td>
+                    <td className="border-y-2 px-4 py-2 border-indigo-600">{user.email}</td>
+                    <td className="border-y-2 px-4 py-2 border-indigo-600">{user.id}</td>
+                    <td className="border-y-2 px-4 py-2 border-indigo-600">
+                        <img className="size-40 transition-transform duration-1000 transform hover:scale-110 max-w-none"
+                            src={user.avatar}/></td>
+                    <td className="border-y-2 px-4 py-2 border-indigo-600">
+                    <div className="inline-flex rounded-md shadow-sm" role="group">
+                        <Link to={`/user/${user._id}`} 
+                                className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border
+                                             border-gray-900 rounded-s-lg hover:bg-gray-900 hover:text-white 
+                                             focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white
+                                             dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">Edit</Link>
+                        <button type="button" className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border-t border-b border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700">
+                            Delete
+                        </button>
+                    </div>
+                    </td>
                 </tr>
-                <tr>
-                    <td className="border px-4 py-2">Angela</td>
-                    <td className="border px-4 py-2">Cardona</td>
-                    <td className="border px-4 py-2">angela@correo.com</td>
-                    <td className="border px-4 py-2">454545455</td>
-                    <td className="border px-4 py-2"><img className="size-40 transition-transform duration-300 transform hover:scale-110" 
-                    src="https://upload.wikimedia.org/wikipedia/commons/5/53/Amparo_Grisales_2023_01.jpg"/></td>
-                </tr>
+                ))}
             </tbody>
         </table>
         </div>
